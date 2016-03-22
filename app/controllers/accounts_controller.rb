6e51f1da7d1111ed0a3b7a9e4c96dcda0c3a040d
giftofjehovah <jonathan.pwh@gmail.com>
2016-03-22 11:28:56 +0800

    create account page

--- app/controllers/accounts_controller.rb
+++ app/controllers/accounts_controller.rb
@@ -1,4 +1,17 @@
 class AccountsController < ApplicationController
   def index
+  	@accounts = Account.where(user_id: current_user.id)
+  	@bank = []
+  	@cash = []
+  	@credit = []
+  	@accounts.each do |account|
+  		if account.type_of_account == 'bank'
+  			@bank << account
+  		elsif account.type_of_account == 'cash'
+  			@cash << account
+  		elsif account.type_of_account == 'credit'
+  			@credit << account
+  		end
+  	end
   end
 end
