f195ccf62adc11c6b82b8711c597cbb8abfb7334
giftofjehovah <jonathan.pwh@gmail.com>
2016-03-22 21:10:42 +0800

    create summary index

--- app/controllers/summary_controller.rb
+++ app/controllers/summary_controller.rb
@@ -1,4 +1,13 @@
 class SummaryController < ApplicationController
   def index
+  	@summary = []
+  	summaries = Catergory.all
+  	summaries.each do |summary|
+  		hash = {
+  			catergory: summary.name,
+  			total: summary.transactions.sum(:amount)
+  		}
+  		@summary << hash
+  	end
   end
 end
